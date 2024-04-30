c5c96b5255bfc87fcdb9894ab0e9b19359912b7d
Nick Sanchez <nicksanchez2371@gmail.com>
2024-04-30 00:49:44 -0500

    .addEventlistener didnt work because there was no matching form id in html

---
+++ main.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('name');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
