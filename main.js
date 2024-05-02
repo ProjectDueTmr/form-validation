603419513edcc813b92f00db3b49da88ce6906b3
Nick Sanchez <nicksanchez2371@gmail.com>
2024-05-01 22:45:46 -0500

    name now is required

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ const form = document.getElementById('form');
 const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    let messages = [];
+    if (name.value === '' || name.value == null) {
+        messages.push('Name is required');
+    }
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(', ')
+    }
 })
