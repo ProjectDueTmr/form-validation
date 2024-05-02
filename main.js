cd517fdef9ee9ca7113d0d590bf1c4bdbf4fcbc7
Nick Sanchez <nicksanchez2371@gmail.com>
2024-05-01 23:17:01 -0500

    finished

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ form.addEventListener('submit', (e) => {
     if (name.value === '' || name.value == null) {
         messages.push('Name is required');
     }
+    if(password.value.length <= 6){
+        messages.push('Password must be longer than 6 characters');
+    }
+    if(password.value.length >= 15){
+        messages.push('Password must be shorter than 15 characters');
+    }
     if(messages.length > 0){
         e.preventDefault()
         errorElement.innerText = messages.join(', ')
